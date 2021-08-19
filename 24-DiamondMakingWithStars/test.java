57999c7220e1c5846ddedadb176ee3b9e46a9fd3
fatihari <57245919+fatihari@users.noreply.github.com>
2021-08-19 20:13:22 +0300

    First Commit

---
+++ 24-DiamondMakingWithStars/test.java
@@ -0,0 +1,45 @@
+package DiamondMakingWithStars;
+/**
+ *  @author Fatih ARI - 19.08.2021
+ * 
+ * A diamond shape with stars is designed using for loops.
+ 
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    * 
+
+ */
+
+public class test {
+    public static void main(String[] args) 
+    {
+        for (int i = 1; i <= 5; i++) 
+        {            
+            for (int j = 5-i; j >= 0 ; j--) 
+            {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2*i-1); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = 4; i >= 1; i--) 
+        {            
+            for (int j = 0; j <= 5-i ; j++) 
+            {
+                System.out.print(" ");
+            }
+            for (int j = (2*i-1); j >= 1 ; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
